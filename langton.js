58fe174b5d39ec78213a4f116d2806b34477b816
Stefan Weigert <weigert.stefan@gmail.com>
2021-11-18 20:58:36 +0100

    js version

--- langton.js
+++ langton.js
@@ -13,8 +13,8 @@ const direction = {
     left: 3
 };
 
-const WIDTH = 800;
-const HEIGHT = 800;
+const WIDTH = 500;
+const HEIGHT = 500;
 
 class Ant {
     constructor(rule) {
@@ -24,40 +24,84 @@ class Ant {
         this.y = HEIGHT / 2;
         //this.x = Math.floor(Math.random() * WIDTH);
         //this.y = Math.floor(Math.random() * HEIGHT);
+
+        this.moves = 0
+        this.mutationSpeed = Math.max(5, Math.floor(Math.random() * 20));
+        this.mutationAmount = Math.floor(Math.random() * Math.max(1, Math.floor(this.rule.length/10)));
     }
 
-    move() {
+    move(cells) {
+        this.moves++;
+        var newX = this.x;
+        var newY = this.y;
+        this.direction = (this.direction + this.rule[cells[this.x][this.y]]) % 4
         switch (this.direction) {
             case direction.up:
-                this.y = (this.y + (HEIGHT - 1)) % HEIGHT;
+                newY = (this.y + (HEIGHT - 1)) % HEIGHT;
                 break;
             case direction.right:
-                this.x = (this.x + 1) % WIDTH;
+                newX = (this.x + 1) % WIDTH;
                 break;
             case direction.down:
-                this.y = (this.y + 1) % HEIGHT;
+                newY = (this.y + 1) % HEIGHT;
                 break;
             case direction.left:
-                this.x = (this.x + (WIDTH - 1)) % WIDTH;
+                newX = (this.x + (WIDTH - 1)) % WIDTH;
                 break;
             default:
-                alert("Unknown direction: " + this.direction);
+                console.log("Unknown direction: " + this.direction);
         }
+
+        this.x = newX;
+        this.y = newY;
+        // we moved
+        return true;
+        /*
+        if (Math.abs(cells[newX][newY] - cells[this.x][this.y]) < 2 || cells[newX][newY] == -1) {
+            this.x = newX;
+            this.y = newY;
+            return true;
+        }
+
+        return false;*/
+
+        /*
+        if (this.moves > (this.rule.length * this.mutationSpeed)) {
+            // console.log("Mutate; Speed:", this.mutationSpeed, "; Amount: ", this.mutationAmount);
+            for (var i = 0; i < this.mutationAmount; i++) {
+                var idx = Math.floor(Math.random() * this.rule.length);
+                var newRule = [direction.left, direction.right][Math.floor(Math.random() * 2)];
+                // console.log("\tMutate - idx ", idx, " from ", this.rule[idx] + " to " + newRule);
+                this.rule[idx] = newRule;
+            }
+
+            this.moves = 0;
+        }
+        */
     }
 }
 
+// LRLRLRRR
+
 var cells = [];
 var ants = [];
+var stopped = true;
 
 function init() {
+    counter = 0;
+    stopped = false;
     for (var i = 0; i < HEIGHT; i++) {
         cells[i] = [];
         for (var j = 0; j < WIDTH; j++) {
-            cells[i][j] = 0;
+            cells[i][j] = -1;
         }
     }
 
-    addAnt();
+    addAnt(1);
+}
+
+function stop() {
+    stopped = true;
 }
 
 function genRule() {
@@ -75,8 +119,12 @@ function genRule() {
     return ruleStr;
 }
 
+function addAntClicked() {
+    cnt = document.getElementById('antCntChooser').value;
+    addAnt(cnt)
+}
 
-function addAnt() {
+function addAnt(cnt) {
     ruleStr = document.getElementById('rule').value;
     if (ruleStr == "") {
         ruleStr = genRule();
@@ -96,48 +144,104 @@ function addAnt() {
         }
     }
 
-    ants.push(new Ant(rule));
+    for (i = 0; i < cnt; i++) {
+        ants.push(new Ant(rule));
+    }
 
     update();
 }
 
 
-needsUpdate = [];
-iterations = 0;
+needsRedraw = [];
 
 /**
  * Check which cells are still alive.
  */
 function update(cnt) {
+    if (stopped) {
+        return
+    }
+
+    counter += 1
+
     ants.forEach(function (ant, i) {
         cellState = cells[ant.x][ant.y]
-        ant.direction = (ant.direction + ant.rule[cellState]) % 4
+        if (cellState == -1) {
+            cellState = 0;
+        }
+        
         cells[ant.x][ant.y] = (cellState + 1) % ant.rule.length
-        needsUpdate.push([ant.x, ant.y])
-
-        ant.move()
-        needsUpdate.push([ant.x, ant.y])
+        
+        needsRedraw.push([ant.x, ant.y, (cellState + 1) % ant.rule.length])
+        if (ant.move(cells)) {
+            needsRedraw.push([ant.x, ant.y, cells[ant.x][ant.y]]);
+        }
     });
 
     draw();
 }
 
-var canvas = document.getElementById('gol').getContext('2d');
+var canvas = document.getElementById('canvas').getContext('2d');
 var seq = [];
+var currentPal = "";
+var currentStart = -1;
+var currentEnd = -1;
+
 /**
  * Draw cells on canvas
  */
 function draw() {
-    if (seq.length == 0) {
-        seq = palette('sequential', ants[0].rule.length);
+    var selectedStart = document.getElementById('rule').selectionStart;
+    var selectedEnd = document.getElementById('rule').selectionEnd;
+    var selectedPal = document.getElementById('paletteChooser').value;
+    
+    var redraw = false;
+    if (currentPal == "" || currentPal != selectedPal) {
+        currentPal = selectedPal;
+        if (currentPal == "grayscale") {
+            seq = chroma.scale().mode('lab').domain([0,ants[0].rule.length]);    
+        } else {
+            seq = chroma.scale(currentPal).mode('lab').domain([0,ants[0].rule.length]);
+        }
+        redraw = true;
     }
-    needsUpdate.forEach(function (coords, c) {
+    if (currentStart == -1 || currentStart != selectedStart) {
+        currentStart = selectedStart;
+        redraw = true;
+    }
+    if (currentEnd == -1 || currentEnd != selectedEnd) {
+        currentEnd = selectedEnd;
+        redraw = true;
+    }
+
+
+    if (redraw) {
+        canvas.clearRect(0, 0, WIDTH, HEIGHT);
+        for (x = 0; x < WIDTH; x++) {
+            for (y = 0; y < HEIGHT; y++) {
+                if (cells[x][y] >= currentStart && cells[x][y] <= currentEnd) {
+                    canvas.fillStyle = seq(cells[x][y]).hex();
+                    canvas.fillRect(x * 1, y * 1, 1, 1);                
+                }
+            }
+        }
+    }
+
+    needsRedraw.forEach(function (coords, c) {
         x = coords[0];
         y = coords[1];
-        canvas.fillStyle = "#" + seq[cells[x][y]];
+        
+        //cellState = coords[2];
+        //cells[x][y] = cellState;
+
+        if (cells[x][y] >= currentStart && cells[x][y] <= currentEnd) {
+            canvas.fillStyle = seq(cells[x][y]).hex();
+        } else {
+            canvas.fillStyle = "#000000";
+        }
         canvas.fillRect(x * 1, y * 1, 1, 1);
     });
-    needsUpdate = [];
+    needsRedraw = [];
 
     setTimeout(function () { update(); }, 0);
 }
